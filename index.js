a174af59fa5717124d8f9b4bcad067f4b5819c49
liuquanyou <liuquanyou@baidu.com>
2017-09-01 11:04:59 +0800

    fixed config bug.

--- index.js
+++ index.js
@@ -7,16 +7,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const router = new Router();
+const fileInfos = require('./src/files');
 const defaultPort = 8910;
 
 module.exports = (config = {}) => {
     const port = config.port || defaultPort;
     const app = new Koa();
+    let files = fileInfos(config);
     app.use(router.routes());
     app.use(router.allowedMethods());
     app.listen(port);
     return {
         app,
-        router
+        router,
+        files
     };
 };
--- src/files.js
+++ src/files.js
@@ -6,77 +6,107 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
+/**
+ * 获取文件目录树
+ *
+ * @params {Object} config 遍历文档目录所需配置
+ * @params {path} config.dirs 需要遍历的文档的路径
+ * @params {Array} config.dirname 目录名字对应信息
+ * @params {Array} config.ignoreDir 遍历时需要忽略的文档
+ * @return {Object} 文件目录树对象
+ * */
+module.exports = (config = {}) => {
+    let dirs = config.path;
+    let dirname = config.dirname || [];
+    let walkArr = [];
+    let dirnameMap = {};
+    let ignorDor = config.ignoreDir || [];
 
-    /**
-     * 获取文件目录树
-     *
-     * @params {string} dirs 文档根目录路径
-     * @params {string} dirname 文档名称数据
-     * @return {Object} 文件目录树
-     * */
-    walker(dirs, dirname) {
-        let me = this;
-        let walkArr = [];
-        let dirnameMap = {};
-        let confDirname = dirname || [];
-        let ignorDor = config.get('ignoreDir');
-        docWalker(dirs, walkArr);
-        function docWalker(dirs, dirCtt) {
-            let dirArr = fs.readdirSync(dirs);
-            dirArr = dirArr || [];
-            dirArr.forEach(it => {
-                let childPath = path.join(dirs, it);
-                let stat = fs.statSync(childPath);
-                let relPath = childPath.replace(config.get('path'), '');
-                // 如果是文件夹就递归查找
-                if (stat.isDirectory()) {
-
-                    // 如果是配置中忽略的目录,则跳过
-                    if (ignorDor.indexOf(it) === -1) {
-                        // 文件夹设置名称获取
-                        let crtName = it || '';
-
-                        for (let index = 0, length = confDirname.length; index < length; index++) {
-                            let dnItems = confDirname[index];
-                            if (dnItems[it]) {
-                                crtName = dnItems[it].name;
-                                dirnameMap[it] = crtName;
-                                break;
-                            }
-                        }
-
-                        // 如果没有配置文件夹目录名称,则不显示
-                        let childArr = [];
-                        dirCtt.push({
-                            itemName: it,
-                            type: 'dir',
-                            path: relPath,
-                            displayName: crtName,
-                            child: childArr
-                        });
-                        docWalker(childPath, childArr);
-                    }
-                }
-                // 如果是文件
-                else {
-                    if (/^\.md$|html$|htm$/i.test(path.extname(it))) {
-                        let basename = path.basename(it, path.extname(it));
-                        let title = me.getMdTitle(childPath);
-                        dirCtt.push({
-                            itemName: basename,
-                            type: 'file',
-                            path: relPath,
-                            title: title
-                        });
-                    }
-                }
-            });
-        }
-
-        return {
-            walkArr,
-            dirnameMap
-        };
+    if (typeof dirs !== 'string') {
+        throw new Error('please input valid dirs path.');
     }
+
+    docWalker(dirs, walkArr);
+    function docWalker(dirs, dirCtt) {
+        let dirArr = fs.readdirSync(dirs);
+        dirArr = dirArr || [];
+        dirArr.forEach(it => {
+            let childPath = path.join(dirs, it);
+            let stat = fs.statSync(childPath);
+            let relPath = childPath.replace(dirs || '', '');
+            // 如果是文件夹就递归查找
+            if (stat.isDirectory()) {
+
+                // 如果是配置中忽略的目录,则跳过
+                if (ignorDor.indexOf(it) === -1) {
+                    // 文件夹设置名称获取
+                    let crtName = it || '';
+
+                    for (let index = 0, length = dirname.length; index < length; index++) {
+                        let dnItems = dirname[index];
+                        if (dnItems[it]) {
+                            crtName = dnItems[it].name;
+                            dirnameMap[it] = crtName;
+                            break;
+                        }
+                    }
+
+                    // 如果没有配置文件夹目录名称,则不显示
+                    let childArr = [];
+                    dirCtt.push({
+                        itemName: it,
+                        type: 'dir',
+                        path: relPath,
+                        displayName: crtName,
+                        child: childArr
+                    });
+                    docWalker(childPath, childArr);
+                }
+            }
+            // 如果是文件
+            else {
+                if (/^\.md$|html$|htm$/i.test(path.extname(it))) {
+                    let basename = path.basename(it, path.extname(it));
+                    let title = getMdTitle(childPath);
+                    dirCtt.push({
+                        itemName: basename,
+                        type: 'file',
+                        path: relPath,
+                        title: title
+                    });
+                }
+            }
+        });
+    }
+
+    return {
+        walkArr,
+        dirnameMap
+    };
 };
+
+/**
+ * 获取markdown文件大标题
+ *
+ * @params {string} dir markdown文件的路径
+ * @return {string} markdown文件大标题
+ * */
+function getMdTitle(dir) {
+    if (!dir) {
+        return '';
+    }
+    let titleArr = [];
+    let ext = path.extname(dir);
+    dir = decodeURIComponent(dir);
+    let content = fs.readFileSync(dir).toString();
+
+    if (ext === '.md') {
+        titleArr =  /^\s*#+\s?([^#\r\n]+)/.exec(content) || [];
+        return titleArr[1] || '';
+    }
+    else if (ext === '.html' || ext === '.htm') {
+        titleArr = /<title>(.+?)<\/title>/.exec(content) || [];
+        return titleArr[1] || '';
+    }
+    return '';
+}
